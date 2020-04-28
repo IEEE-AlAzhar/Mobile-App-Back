ba705cb735687220745f9cb7d9d8e2cb2ec88a0c
Esmail <elmoussel12@gmail.com>
2020-04-28 22:53:53 +0200

    Adding user model!

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const path = require("path");
 
 const app = express();
 
+// require Models
+const User = require("./models/User.model");
+
 // require controllers
 const handleLogin = require("./controllers/user/handleLogin");
 const getUser = require("./controllers/user/getUser");
@@ -28,10 +31,10 @@ app.use(cors());
 
 // end-points
 app.get("/", (req, res) => res.json("root is working!"));
-app.post("/login", handleLogin("User"));
-app.get("/user/:id", getUser("User"));
-app.put("/user/:id/image", handleUserImage("User"));
-app.put("/user/:id/phone", handleUserPhone("User"));
+app.post("/login", handleLogin(User));
+app.get("/user/:id", getUser(User));
+app.put("/user/:id/image", handleUserImage(User));
+app.put("/user/:id/phone", handleUserPhone(User));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "public")));
---
+++ models/User.model.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+// Create feedback Schema
+const FeedbackSchema = new Schema({
+    author: {
+        type: String,
+        required: [true,'author is required!']
+    },
+    content: {
+        type: String,
+        required: [true,'content is required!']
+    }
+})
+
+// Create achievement Schema
+const AchievementSchema = new Schema({
+    title: {
+        type: String,
+        required: [true,'title is required!']
+    },
+    description: {
+        type: String,
+        required: [true,'description is required!']
+    }
+})
+
+
+// creating user schema
+const userSchema = new Schema ({
+    code: {
+        type: Number,
+        required: [true,'User code is required!']
+    },
+    name: {
+        type: String,
+        required: [true,'Username is required!']
+    },
+    phone: {
+        type: Number,
+        required: [true,'Phone number is required!']
+    },
+    image: {
+        type: String
+    },
+    role: {
+        type: String,
+        required: [true,'user role is required!']
+    },
+    type: {
+        type: String,
+        required: [true,'user type is required!']
+    },
+    committee: {
+        type: String,
+        required: [true,'user committee is required!']
+    },
+    feedbacks: FeedbackSchema,
+    achievements: AchievementSchema
+})
+
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
