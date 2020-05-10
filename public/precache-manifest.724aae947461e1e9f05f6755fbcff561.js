c016375998920c2b0493d93a17bdc3725e894fba
mohamedsaad <mahameds19999@gmail.com>
2020-05-10 05:00:06 +0200

    Edits

---
+++ public/precache-manifest.724aae947461e1e9f05f6755fbcff561.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1adebd91519119f42f99839aac20ec82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "322bec3d573ad4054d1b",
+    "url": "/static/css/2.9638f6db.chunk.css"
+  },
+  {
+    "revision": "14b5e57bfb44a74a97c9",
+    "url": "/static/css/main.dd935a8d.chunk.css"
+  },
+  {
+    "revision": "322bec3d573ad4054d1b",
+    "url": "/static/js/2.665e5b41.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.665e5b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14b5e57bfb44a74a97c9",
+    "url": "/static/js/main.b9093a83.chunk.js"
+  },
+  {
+    "revision": "978c61ddd7cb3d7fc0fb",
+    "url": "/static/js/runtime-main.875d77ee.js"
+  }
+]);
